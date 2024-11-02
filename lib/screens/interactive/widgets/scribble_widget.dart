8e763b3ae6280d5752fac49fd68f00f314418377
Shotaro Tao <taoshotaro@users.noreply.github.com>
2024-11-02 16:31:02 +0900

    fix

--- lib/screens/interactive/widgets/scribble_widget.dart
+++ lib/screens/interactive/widgets/scribble_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:gottani_mobile/features/thermal.dart';
 import 'package:gottani_mobile/screens/interactive/painter/glow_path_painter.dart';
 
 class ScribbleWidget extends HookWidget {
@@ -14,17 +15,31 @@ class ScribbleWidget extends HookWidget {
     final scale = useState<double>(1);
     final points = useState<List<Offset>>([]);
 
+    final throttler = useState(ThermalThrottler(
+      onScratchBegan: (offset, time, deltaHeat) {},
+      onScratchMoved: (offset, time, deltaHeat) {
+        debugPrint("DEBUG: offset $offset time $time deltaHeat $deltaHeat");
+      },
+      onScratchEnded: (offset, time, deltaHeat) {},
+    ));
+
     void onLongPressStart(LongPressStartDetails details) {
+      throttler.value.start(details.localPosition, ThermalTime.now());
+
       HapticFeedback.lightImpact();
       scale.value = 1.2;
     }
 
     void onLongPressEnd(LongPressEndDetails details) {
+      throttler.value.end(details.localPosition, ThermalTime.now());
+
       scale.value = 1.0;
       points.value = [];
     }
 
     void onLongPressMoveUpdate(LongPressMoveUpdateDetails details) {
+      throttler.value.move(details.localPosition, ThermalTime.now());
+
       points.value = [...points.value, details.localPosition];
     }
 
