68a2b9f0be6f83a2116eeabc90e6d11696b2523a
Shotaro Tao <taoshotaro@users.noreply.github.com>
2024-11-02 18:22:55 +0900

    fix

--- lib/screens/interactive/widgets/comment_widget.dart
+++ lib/screens/interactive/widgets/comment_widget.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
 class CommentWidget extends StatelessWidget {
-  const CommentWidget({super.key});
+  const CommentWidget({super.key, this.shadow = false});
+
+  final bool shadow;
 
   @override
   Widget build(BuildContext context) {
@@ -14,13 +16,22 @@ class CommentWidget extends StatelessWidget {
             vertical: 24,
             horizontal: 32,
           ),
-          decoration: const BoxDecoration(
+          decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.only(
               topLeft: Radius.circular(16),
               topRight: Radius.circular(16),
               bottomRight: Radius.circular(16),
             ),
+            boxShadow: [
+              if (shadow)
+                BoxShadow(
+                  color: Color(0xffD24148), // 影の色と透明度
+                  spreadRadius: 10, // 影の広がり具合
+                  blurRadius: 40, // 影のぼかし具合
+                  offset: Offset(0, 0), // 影の位置（x方向、y方向）
+                ),
+            ],
           ),
           child: Text(
             "告られた",
--- lib/screens/interactive/widgets/scribble_widget.dart
+++ lib/screens/interactive/widgets/scribble_widget.dart
@@ -1,26 +1,37 @@
+import 'dart:math';
+
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gottani_mobile/features/thermal.dart';
 import 'package:gottani_mobile/screens/interactive/painter/glow_path_painter.dart';
+import 'package:gottani_mobile/screens/interactive/widgets/comment_widget.dart';
 
 class ScribbleWidget extends HookWidget {
-  const ScribbleWidget({super.key, this.child});
-
-  final Widget? child;
+  const ScribbleWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     final scale = useState<double>(1);
     final points = useState<List<Offset>>([]);
+    final shakeOffset = useState<Offset>(Offset.zero);
+    final shouldAnimate = useState(false);
 
     final throttler = useState(ThermalThrottler(
       onScratchBegan: (offset, time, deltaHeat) {},
       onScratchMoved: (offset, time, deltaHeat) {
         debugPrint("DEBUG: offset $offset time $time deltaHeat $deltaHeat");
+
+        if (deltaHeat > 0.2) {
+          shouldAnimate.value = true;
+        } else {
+          shouldAnimate.value = false;
+        }
       },
-      onScratchEnded: (offset, time, deltaHeat) {},
+      onScratchEnded: (offset, time, deltaHeat) {
+        shakeOffset.value = Offset.zero; // 振動を停止
+      },
     ));
 
     void onLongPressStart(LongPressStartDetails details) {
@@ -33,8 +44,11 @@ class ScribbleWidget extends HookWidget {
     void onLongPressEnd(LongPressEndDetails details) {
       throttler.value.end(details.localPosition, ThermalTime.now());
 
+      shouldAnimate.value = false;
+
       scale.value = 1.0;
       points.value = [];
+      shakeOffset.value = Offset.zero;
     }
 
     void onLongPressMoveUpdate(LongPressMoveUpdateDetails details) {
@@ -62,18 +76,70 @@ class ScribbleWidget extends HookWidget {
         scale: scale.value,
         duration: const Duration(milliseconds: 200), // アニメーションの速度
         curve: Curves.easeOut, // アニメーションのカーブ
-        child: Stack(
-          children: [
-            if (child != null) child!,
-            Opacity(
-              opacity: 0.5,
-              child: CustomPaint(
-                painter: GlowPathPainter(points.value),
+        child: ShakeRotationAnimation(
+          shouldAnimate: shouldAnimate,
+          child: Stack(
+            children: [
+              CommentWidget(
+                shadow: shouldAnimate.value,
               ),
-            ),
-          ],
+              Opacity(
+                opacity: 0.5,
+                child: CustomPaint(
+                  painter: GlowPathPainter(points.value),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class ShakeRotationAnimation extends HookWidget {
+  final Widget child;
+  final Duration duration;
+  final ValueNotifier<bool> shouldAnimate;
+
+  const ShakeRotationAnimation({
+    Key? key,
+    required this.child,
+    this.duration = const Duration(milliseconds: 300),
+    required this.shouldAnimate,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = useAnimationController(
+      duration: Duration(milliseconds: 100),
+      initialValue: Random().nextDouble(),
+    );
+    final animation = useMemoized(
+      () => Tween<double>(begin: -0.1, end: 0.1)
+          .chain(CurveTween(curve: Curves.linear))
+          .animate(controller),
+      [controller],
+    );
+
+    useEffect(() {
+      if (shouldAnimate.value && !controller.isAnimating) {
+        controller.repeat(reverse: true);
+      } else {
+        controller.stop();
+      }
+      return null;
+    }, [shouldAnimate.value]);
+
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (context, child) {
+        return Transform.rotate(
+          angle: animation.value, // 少し回転
+          child: this.child,
+        );
+      },
+      child: child,
     );
   }
 }
