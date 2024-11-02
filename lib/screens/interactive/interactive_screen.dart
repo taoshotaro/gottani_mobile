ae6859148690e157b4cdfb20534d6701111246ce
Kitsunesaki Mezumona <mezumona@gmail.com>
2024-11-02 19:59:19 +0900

    Bind messages.

--- lib/screens/interactive/interactive_screen.dart
+++ lib/screens/interactive/interactive_screen.dart
@@ -1,77 +1,112 @@
+import 'dart:async';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gottani_mobile/models/message.dart';
+import 'package:gottani_mobile/repositories/message_repository.dart';
 import 'package:gottani_mobile/screens/interactive/painter/grid_dots_painter.dart';
-import 'package:gottani_mobile/screens/interactive/widgets/comment_widget.dart';
 import 'package:gottani_mobile/screens/interactive/widgets/scribble_widget.dart';
 import 'package:gottani_mobile/screens/interactive/widgets/snapping_interactive_viewer.dart';
 
 @immutable
 @RoutePage()
-class InteractiveScreen extends HookWidget {
+class InteractiveScreen extends ConsumerStatefulWidget {
   const InteractiveScreen({
     super.key,
   });
 
   @override
+  ConsumerState<InteractiveScreen> createState() => InteractiveScreenState();
+}
+
+class InteractiveScreenState extends ConsumerState<InteractiveScreen> {
+  late final Key viewerKey;
+  final List<Widget> messageWidgets = [];
+
+  @override
+  void initState() {
+    super.initState();
+    viewerKey = GlobalKey();
+
+    unawaited(Future.microtask(() async {
+      final messages = await ref.read(
+        latestMessagesProvider(20).future,
+      );
+      if (!context.mounted) {
+        return;
+      }
+
+      messageWidgets.addAll(messages.map(
+        (message) => _PositionedScribbleWidget(
+          key: ValueKey(message.id),
+          message: message,
+        ),
+      ));
+      setState(() {});
+    }));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final globalKey = useMemoized(() => GlobalKey());
+    ref.listen(newMessageStreamProvider.future, (_, message) {
+      message.then((message) {
+        if (context.mounted) {
+          return;
+        }
+        messageWidgets.add(
+          _PositionedScribbleWidget(
+            key: ValueKey(message.id),
+            message: message,
+          ),
+        );
+        setState(() {});
+      });
+    });
 
     return Scaffold(
       body: SnappingInteractiveViewer(
-        key: globalKey,
+        key: viewerKey,
         child: Container(
           width: 2000,
           height: 2000,
           color: Colors.black,
           child: CustomPaint(
             painter: GridDotsPainter(),
-            child: Stack(
-              children: [
-                Positioned(
-                  left: 100,
-                  top: 50,
-                  child: ScribbleWidget(),
-                ),
-                Positioned(
-                  left: 500,
-                  top: 100,
-                  child: ScribbleWidget(),
-                ),
-                Positioned(
-                  left: 900,
-                  top: 100,
-                  child: ScribbleWidget(),
-                ),
-                Positioned(
-                  left: 1500,
-                  top: 150,
-                  child: ScribbleWidget(),
-                ),
-                Positioned(
-                  left: 100,
-                  top: 350,
-                  child: ScribbleWidget(),
-                ),
-                Positioned(
-                  left: 500,
-                  top: 400,
-                  child: ScribbleWidget(),
-                ),
-                Positioned(
-                  left: 900,
-                  top: 400,
-                  child: ScribbleWidget(),
-                ),
-                Positioned(
-                  left: 1500,
-                  top: 450,
-                  child: ScribbleWidget(),
-                ),
-              ],
-            ),
+            child: Stack(children: messageWidgets),
           ),
         ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(children: [
+          TextButton(
+            onPressed: () => ref
+                .read(messageRepositoryProvider)
+                .send('added: ${DateTime.now().toIso8601String()}'),
+            child: const Text('add'),
+          )
+        ]),
+      ),
+    );
+  }
+}
+
+@immutable
+class _PositionedScribbleWidget extends StatelessWidget {
+  const _PositionedScribbleWidget({
+    super.key,
+    required this.message,
+  });
+
+  final Message message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      child: ScribbleWidget(
+        id: message.id,
+        content: message.content,
+        initialHeat: message.heat,
       ),
     );
   }
