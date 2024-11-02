658090eaaa111d6ce4d5ae2d84c765ef911dba75
natsukiinaba <nackyinaba@gmail.com>
2024-11-02 17:36:08 +0900

    fix: messageRepo

--- lib/repositories/message_repository.dart
+++ lib/repositories/message_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gottani_mobile/datasources/supabase_client.dart';
 import 'package:gottani_mobile/models/message.dart';
@@ -7,11 +9,28 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 part 'message_repository.g.dart';
 
 @riverpod
-Stream<List<Message>> messageStream(Ref ref) {
+Stream<Message> newMessageStream(Ref ref) {
   final supabase = ref.read(supabaseClientProvider);
 
-  return supabase.from('Message').stream(primaryKey: ['id']).map(
-      (data) => data.map((json) => Message.fromJson(json)).toList());
+  final streamController = StreamController<Message>();
+
+  final subscription = supabase
+      .channel('public:message')
+      .onPostgresChanges(
+          event: PostgresChangeEvent.insert,
+          schema: 'public',
+          table: 'message',
+          callback: (payload) {
+            streamController.add(Message.fromJson(payload.newRecord));
+          })
+      .subscribe();
+
+  ref.onDispose(() {
+    unawaited(subscription.unsubscribe());
+    unawaited(streamController.close());
+  });
+
+  return streamController.stream;
 }
 
 @riverpod
@@ -22,6 +41,12 @@ class MessageRepository extends _$MessageRepository {
   }
 
   late final SupabaseClient supabase;
+
+  Future<List<Message>> fetchMessages(int count) async {
+    final response = await supabase.from('Message').select().limit(count);
+
+    return response.map((json) => Message.fromJson(json)).toList();
+  }
 
   Future<void> createMessage(String content) async {
     final response = await supabase.from('Message').insert({
